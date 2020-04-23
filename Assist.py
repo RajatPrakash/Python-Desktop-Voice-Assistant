b7ab3c345248282c7a24edb6ba6e54faa7203773
Rajat Prakash <rajatprakash904@gmail.com>
2020-04-23 10:32:01 +0530

    Code updated , Added online tv and some more basics commands .

--- Assist.py
+++ Assist.py
@@ -7,6 +7,9 @@ import Restart as r
 import webbrowser
 import os
 import Shut as s
+import sys
+
+sys.path.append('..')
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -66,42 +69,56 @@ if __name__ == '__main__':
             results = wikipedia.summary(query, sentences=3)
             speak('According to Wikipedia')
             speak(results)
+        elif 'who are you' in query:
+            speak('I am your Personal Assistant designed to make your work simpler')
         elif 'youtube' in query:
             url = 'www.youtube.com'
             webbrowser.get('chrome').open_new_tab(url)
+            speak('Opening youtube ')
         elif 'netflix' in query:
             url = 'www.netflix.com'
             webbrowser.get('chrome').open_new_tab(url)
+            speak('Opening Netflix ')
         elif 'amazon' in query:
             url = 'www.amazon.com'
             webbrowser.get('chrome').open_new_tab(url)
+            speak('Opening Amazon shopping website ')
+        elif 'tata sky' in query:
+            url = 'https://watch.tatasky.com/live-tv?pageType=live-tv'
+            webbrowser.get('chrome').open_new_tab(url)
+            speak('Opening Tata Sky ')
         elif 'play music' in query:
             music = 'C:\\Users\\rajat\\Downloads\\Music'
             songs = os.listdir(music)
             os.startfile(os.path.join(music, songs[0]))
+            speak('Playing Music ')
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime('%H:%M')
             speak(f'sir, The time is {strTime}')
         elif 'google' in query:
             ChromePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
             os.startfile(ChromePath)
+            speak('Opening google ')
         elif 'python' in query:
             PythonPath = "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2019.2\\bin\\pycharm64.exe"
             os.startfile(PythonPath)
-        elif 'sql' in query:
+            speak('Opening Python Please Wait ')
+        elif 'sequel server' in query:
             SqlPath = "C:\\Program Files (x86)\\Microsoft SQL Server Management Studio 18\\Common7\\IDE\\Ssms.exe"
             os.startfile(SqlPath)
-        elif 'mynet' in query:
+            speak('Opening Sequel Server Please Wait ')
+        elif 'my net' in query:
             EdgePath = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
             os.startfile(EdgePath)
+            speak('Opening Edge Browser ')
         elif 'github' in query:
             GitPath = "C:\\Program Files\\Git\\git-bash.exe"
             os.startfile(GitPath)
-        elif 'shutdown please' in query:
+            speak('Opening GitHub ')
+        elif 'shut down please' in query:
             speak('Closing...')
             s.shutdown()
-
         elif 'restart please' in query:
             speak('Restarting...')
             r.restart()
->>>>>>> ad2be76d956136970b716fe45b92469d5141ad22
+
